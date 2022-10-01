32940eca309b76f44e0cb75b8958d8d050ebd191
Wannes Gennar <wannes.gennar@gmail.com>
2022-10-01 11:26:35 +0200

    modify opening slide to match Re:Factor branding

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import Reveal from 'reveal.js';
 import RevealNotes from 'reveal.js/plugin/notes/notes';
 
 const branding = document.getElementById('branding-logo');
+const logo = document.getElementById('refactor-logo');
+const footer = document.getElementById('opening-footer');
+
 const deck = new Reveal({
 	plugins: [RevealNotes],
 	hash: true,
@@ -11,6 +14,24 @@ const deck = new Reveal({
 // ensure the Euricom logo properly transforms to remain readable.
 deck.on('slidechanged', event => {
 	const hasLightBackground = event.currentSlide.dataset.backgroundColor === 'white';
+	const isOpening = event.currentSlide.dataset.opening === 'true';
+
+	deck.configure({ controls: !isOpening });
+	if (isOpening) {
+		footer.classList.remove('animate__fadeOutDown');
+		logo.classList.remove('animate__fadeOut');
+		branding.classList.remove('animate__fadeIn');
+		footer.classList.add('animate__fadeInUp');
+		logo.classList.add('animate__fadeIn');
+		branding.classList.add('animate__fadeOut');
+	} else {
+		footer.classList.remove('animate__fadeInUp');
+		logo.classList.remove('animate__fadeIn');
+		branding.classList.remove('animate__fadeOut');
+		footer.classList.add('animate__fadeOutDown');
+		logo.classList.add('animate__fadeOut');
+		branding.classList.add('animate__fadeIn');
+	}
 
 	if (hasLightBackground) {
 		branding.src = 'assets/euricom-logo-light.png';
